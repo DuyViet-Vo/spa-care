a4ccc4eb9545e9f8253cba1039e9d5d9acf03925
DuyViet-Vo <duyviet1810@gmail.com>
2023-12-21 18:50:14 +0700

    chore: update api lich hen

--- spacare/lich_hen/views.py
+++ spacare/lich_hen/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -13,8 +14,9 @@ class ListCreateLichHenView(ListCreateAPIView):
     queryset = LichHen.objects.all()
     permission_classes = [IsAuthenticated]
     pagination_class = Pagination
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["khach_hanh"]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["khach_hanh", "trang_thai"]
+    search_fields = ["khach_hanh__ho_ten", "khach_hanh__sdt"]
 
     def get_serializer_class(self):
         if self.request.method == "GET":
