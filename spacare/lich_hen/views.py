1f92fd99147f7082a1a96ece6514018d835adb43
DuyViet-Vo <duyviet1810@gmail.com>
2023-12-25 21:46:27 +0700

    chore: paypal api

--- spacare/lich_hen/views.py
+++ spacare/lich_hen/views.py
@@ -1,11 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from spacare.core.pagination import Pagination
 from spacare.lich_hen.models import LichHen
 from spacare.lich_hen.serializers import LichHenSerializer, ReadLichHenSerializer
+from spacare.lich_hen.services import online_payment
+from spacare.users.models import User
 
 
 class ListCreateLichHenView(ListCreateAPIView):
@@ -22,6 +26,29 @@ class ListCreateLichHenView(ListCreateAPIView):
         if self.request.method == "GET":
             return ReadLichHenSerializer
         return LichHenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = LichHenSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            tien_coc = serializer.data["tien_coc"]
+
+            data_khach_hang = User.objects.filter(
+                id=serializer.data["khach_hanh"]
+            ).first()
+            print("++", data_khach_hang.ho_ten)
+            khach_hang = data_khach_hang.ho_ten
+            response = online_payment(tien_coc, khach_hang)
+            if response.status_code == 201:
+                approval_url = response.json()["links"][1]["href"]
+                data_response = {"url": approval_url, "id": serializer.data["id"]}
+                return Response(data_response)
+            else:
+                return Response(response.status_code)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateDeleteLichHenView(RetrieveUpdateDestroyAPIView):
